2cc2ca4e0dec09bf8eae34d5f3a73a6a69b67e78
darlingmota <darlingmotta@gmail.com>
2025-12-04 07:09:40 +0000

    Update full_program.py

--- full_program.py
+++ full_program.py
@@ -77,5 +77,7 @@ def parse_abc_file(path, book_number, file_name):
                     "abc_text": ""
                 }
                 body_lines = []
+
+
 if __name__ == "__main__":
     main()
