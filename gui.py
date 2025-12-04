f52a2f8ba05882a4dd0d4b06281a7f899bc0277a
darlingmota <darlingmotta@gmail.com>
2025-12-04 09:25:54 +0000

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from full_program import(
+    get_connection, create_table, load_all_tunes,
+    load_dataframe, get_tunes_by_book,
+    get_tunes_by_type, search_tunes
+)
